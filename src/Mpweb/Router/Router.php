fdd699af37f32ae20a8035e0af0d6985aad1632c
vagrant <vagrant@ansibleMpwar.(none)>
2017-05-01 19:44:12 +0200

    second commmit

--- src/Mpweb/Router/Router.php
+++ src/Mpweb/Router/Router.php
@@ -5,61 +5,46 @@ namespace Mpweb\Router;
 class Router
 {
 
-const GET = 'GET';
-const POST = 'POST';
 
-private $_routes = array(
-    'GET' => array(),
-    'POST' => array()
-);
+    private $_routes = array();
 
-public function add(Route $route, $method)
-{
-    switch ($method) {
-        case 'GET':
-            $this->_routes['GET'][$route->getURL()] = $route->getController();
-        break;
 
-        case 'POST':
-            $this->_routes['POST'][$route->getURL()] = $route->getController();
-        break;
-
-        default:
-            exit('Error!');
-        break;
+    public function add(Route $route)
+    {
+        $this->_routes[$route->getURL()] = $route->getController();
+             
     }
-}
 
-public function getController(Route $route, $method)
-{
-    return $this->_routes[$method][$route->getURL()];
-}
+    public function delete(Route $route)
+    {
+        unset($this->_routes[$route->getURL()]);
+            
+    }
 
-public function length($method)
-{
-    return count($this->_routes[$method]);
-}
+    public function getController(Route $route)
+    {
+        return $this->_routes[$route->getURL()];
+    }
 
-public function getRoutes()
-{
-    return $this->_routes;
-}
+    public function length()
+    {
+        return count($this->_routes);
+    }
 
-public function getRouteByIndexAndMethod($index, $method)
-{
-    return array_search($index, $this->_routes[$method]);
-}
+    public function getRoutes()
+    {
+        return $this->_routes;
+    }
 
-public function run($path, $method)
-{
-    //$path = array_key_exists('path', $url);
+    public function run($path)
+    {   
+        foreach($this->_routes as $uri => $controller) {
+            
+            if (preg_match($uri, $path, $matches)) {
 
-    foreach (self::$_routes[$method] as $url => $controller) {
-        if (preg_match($url, $path, $matches)) {
-            array_shift($matches);
-            call_user_func_array($callback, array_values($matches));
-            return;
+                return $matches[0];
+            }
+            
         }
     }
-}
 }
--- test/MpwebUnit/Router/RouterTest.php
+++ test/MpwebUnit/Router/RouterTest.php
@@ -9,6 +9,11 @@ use Mpweb\Router\Router;
 
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
+	 private $routes = [ 1 => '/home/',
+	 					 2 => '/post/{id}',
+	 					 3 => '/{category}/{product}/{id}/', 
+	 					 4 => '/tag/{name}/' 
+	 					];
     /**
      * @var Router
      */
@@ -27,10 +32,10 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->router = new Router();
-        $this->route = new Route("/", "@home");
-        $this->route1 = new Route("post/{id}", "@post");
+        $this->route = new Route("~^/home/~", "@home");
+        $this->route1 = new Route("/^\/post\/(?<name>[a-zA-Z0-9\_\-]+)\/?$/", "@post");
+        //preg_match("/^\/post\/(?<name>[a-zA-Z0-9\_\-]+)\/?$/", $input_line, $output_array);
     }
-
 
     protected function tearDown()
     {
@@ -42,24 +47,35 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function shouldHaveZeroElementsWhenNoElementIsAdded()
     {
-        $this->assertEquals(0, $this->router->length("GET"));
-        $this->assertEquals(0, $this->router->length("POST"));
+        $this->assertEquals(0, $this->router->length());
     }
 
      /** @test */
-    public function shouldAddGetRouteToRouter()
+    public function shouldAddRouteToRouter()
     {
-        $this->router->add($this->route, "GET");
-        $this->assertEquals($this->route->getController(), $this->router->getRoutes()["GET"][$this->route->getURL()]);
+        $this->router->add($this->route);
+        $this->router->add($this->route1);
+        $this->assertEquals($this->route->getController(), $this->router->getRoutes()[$this->route->getURL()]);
+        $this->assertEquals($this->route1->getController(), $this->router->getRoutes()[$this->route1->getURL()]);
+
     }
 
     /** @test */
-    public function shouldAddPostRoute()
+    public function shouldGetARoute()
     {
-        $this->router->add($this->route1, "POST");
-        $this->assertEquals($this->route1->getController(), $this->router->getRoutes()["POST"][$this->route1->getURL()]);
+    	$this->router->add($this->route);
+    	$this->router->add($this->route1);
+        $this->assertEquals($this->routes[1] ,$this->router->run("/home/"));
+    
     }
 
+        /** @test */
+    public function shouldGetARoute1()
+    {
+    	$this->router->add($this->route);
+        $this->router->add($this->route1);
+    	$this->assertEquals($this->routes[2] ,$this->router->run("/post/1"));
+    }
 
 
 
