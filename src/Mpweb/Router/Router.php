8a868eccd78644e3471747a33b10c725c3e1bb1b
Alexbeis <vagrant@ansibleMpwar.(none)>
2017-05-04 20:08:54 +0200

    Changes on the matching. Now only checks if true or false

--- src/Mpweb/Router/Router.php
+++ src/Mpweb/Router/Router.php
@@ -4,47 +4,72 @@ namespace Mpweb\Router;
 
 class Router
 {
-    private $_routes = array();
-
-    private $regexExpression = ["~^/home/~",
-                                "/^\/post\/(?<name>[a-zA-Z0-9\_\-]+)\/?$/"];
-
+    private $routes = array();
 
     public function add(Route $route)
     {
-        $this->_routes[] = $route;
+        $this->routes[] = $route;
              
     }
 
     public function delete(Route $route)
     {
-        unset($this->_routes[$route->getURL()]);
+        unset($this->routes[$route->getURL()]);
             
     }
 
     public function getController(Route $route)
     {
-        return $this->_routes[$route->getURL()];
+        return $this->routes[$route->getURL()];
     }
 
     public function length()
     {
-        return count($this->_routes);
+        return count($this->routes);
     }
 
     public function getRoutes()
     {
-        return $this->_routes;
+        return $this->routes;
     }
 
-    public function match($uri, $path)
+    public function match($uri, $regexUri)
     {   
-        foreach($this->_routes as $route) {
-            foreach($this->regexExpression as $regex) {
-                if (preg_match($regex, $route->getURL(), $matches)) {
-                    return true;
-                }
-            }    
+        $params = [];
+
+        preg_match_all('\'' . '{(\w+)}' . '\'', $regexUri, $matches);
+        $matches = $matches[0];
+        foreach ($matches as $key => $value) {
+            $matches[$key] = str_replace('{', '', $matches[$key]);
+            $matches[$key] = str_replace('}', '', $matches[$key]);
         }
+
+        //Replace parameter names to transform URL to regex format.
+        $regexUri = preg_replace('%' . '{(\w+)}' . '%', '(\w+|\d+)', $regexUri);
+        $regexUri .= '$';
+        $regexUri = '%^' . $regexUri . '$%';
+        $res = preg_match($regexUri, $uri, $params);
+    
+        if (!$res || $res == 0) return false;
+
+        $paramLength = count($matches);
+        $keyParams = array();
+
+        for ($i = 0; $i < $paramLength; $i++) {
+            $keyParams[$matches[$i]] = $params[$i + 1];
+        }
+    
+        if ($keyParams) return true;
+
+
+
+
+        // foreach($this->_routes as $route) {
+        //     foreach($this->regexExpression as $regex) {
+        //         if (preg_match($regex, $route->getURL(), $matches)) {
+        //             return true;
+        //         }
+        //     }    
+        // }
     }
 }
--- test/MpwebUnit/Router/RouterTest.php
+++ test/MpwebUnit/Router/RouterTest.php
@@ -10,9 +10,9 @@ use Mpweb\Router\Router;
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
 	
-	 private $routes = [ 1 => '/home/',
-	 					 2 => '/post/{id}',
-	 					 3 => '/{category}/{product}/{id}/', 
+	 private $routes = [ 1 => '/{id}/',
+	 					 2 => '/post/{id}/',
+	 					 3 => '/{category}/{product}/', 
 	 					 4 => '/tag/{name}/' 
 	 					];
     /**
@@ -23,25 +23,39 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     /**
      * @var Route
      */
-    private $route;
+    private $route_1;
 
     /**
      * @var Route
      */
-    private $route1;
+    private $route_2;
+
+      /**
+     * @var Route
+     */
+    private $route_3;
+
+      /**
+     * @var Route
+     */
+    private $route_4;
 
     protected function setUp()
     {
         $this->router = new Router();
-        $this->route = new Route("/home/");
-        $this->route1 = new Route("/post/1");
+        $this->route_1 = new Route("/3/");
+        $this->route_2 = new Route("/post/1/");
+        $this->route_3 = new Route("/una_categoria/un_producto/");
+        $this->route_4 = new Route("/tag/un_producto/");
     }
 
     protected function tearDown()
     {
         $this->router = null;
-        $this->route = null;
-        $this->route1 = null;
+        $this->route_1 = null;
+        $this->route_2 = null;
+        $this->route_3 = null;
+        $this->route_4 = null;
     }
 
     /** @test */
@@ -53,29 +67,40 @@ class RouterTest extends \PHPUnit_Framework_TestCase
      /** @test */
     public function shouldAddRouteToRouter()
     {
-        $this->router->add($this->route);
-        $this->router->add($this->route1);
-        $this->assertEquals($this->route->getURL(), $this->router->getRoutes()[0]->getURL());
-        $this->assertEquals($this->route1->getURL(), $this->router->getRoutes()[1]->getURL());
+        $this->router->add($this->route_1);
+        $this->router->add($this->route_2);
+        $this->router->add($this->route_3);
+        $this->router->add($this->route_4);
+        $this->assertEquals($this->route_1->getURL(), $this->router->getRoutes()[0]->getURL());
+        $this->assertEquals($this->route_2->getURL(), $this->router->getRoutes()[1]->getURL());
+        $this->assertEquals($this->route_3->getURL(), $this->router->getRoutes()[2]->getURL());
+        $this->assertEquals($this->route_4->getURL(), $this->router->getRoutes()[3]->getURL());
 
     }
 
     /** @test */
-    public function shouldGetARoute()
+    public function shouldMatchARoute()
     {
-    	$this->router->add($this->route);
-        $this->router->add($this->route1);
-    	$this->assertTrue($this->router->match($this->routes[1], $this->route));
+    	$this->router->add($this->route_1);
+        $this->router->add($this->route_2);
+        $this->router->add($this->route_3);
+        $this->router->add($this->route_4);
+    	$this->assertTrue($this->router->match($this->route_1->getURL(),$this->routes[1]),"The Route does not match");
+    	$this->assertTrue($this->router->match($this->route_2->getURL(), $this->routes[2]), "The Route does not match");
+    	$this->assertTrue($this->router->match($this->route_3->getURL(), $this->routes[3]), "The Route does not match");
+    	$this->assertTrue($this->router->match($this->route_4->getURL(), $this->routes[4]), "The Route does not match");
+
+
     }
 
 
-    /** @test */
-    public function shouldGetARoute1()
-    {
-    	$this->router->add($this->route);
-    	$this->router->add($this->route1);
-        $this->assertTrue($this->router->match($this->routes[2], $this->route1));
+    // /** @test */
+    // public function shouldGetARoute1()
+    // {
+    // 	$this->router->add($this->route);
+    // 	$this->router->add($this->route1);
+    //     $this->assertTrue($this->router->match($this->routes[2], $this->route1->getURL()));
     
-    }
+    // }
 
 }
