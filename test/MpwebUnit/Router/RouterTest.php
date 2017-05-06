bca25b7ba2a4df406920f4f36bc408f1b33ed37a
Alexbeis <Alexbeis09@gmail.com>
2017-05-06 17:36:58 +0200

    Adding constants

--- test/MpwebUnit/Router/RouterTest.php
+++ test/MpwebUnit/Router/RouterTest.php
@@ -9,12 +9,15 @@ use Mpweb\Router\Router;
 
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
-	
-	 private $routes = [ 1 => '/{id}/',
-	 					 2 => '/post/{id}/',
-	 					 3 => '/{category}/{product}/', 
-	 					 4 => '/tag/{name}/' 
-	 					];
+    const ZERO_ELEMENTS    = 0;
+	const ONE_ELEMENT      = 1;
+    const THREE_ELEMENTS   = 3;
+
+	private $routes = [ 1 => '/{id}/',
+	 					2 => '/post/{id}/',
+	 					3 => '/{category}/{product}/', 
+	 					4 => '/tag/{name}/' 
+	 				  ];
     /**
      * @var Router
      */
@@ -42,7 +45,7 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->router = new Router();
+        $this->router  = new Router();
         $this->route_1 = new Route("/3/");
         $this->route_2 = new Route("/post/1/");
         $this->route_3 = new Route("/una_categoria/un_producto/");
@@ -51,7 +54,7 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 
     protected function tearDown()
     {
-        $this->router = null;
+        $this->router  = null;
         $this->route_1 = null;
         $this->route_2 = null;
         $this->route_3 = null;
@@ -61,7 +64,7 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function shouldHaveZeroElementsWhenNoElementIsAdded()
     {
-        $this->assertEquals(0, $this->router->length());
+        $this->assertEquals(self::ZERO_ELEMENTS, $this->router->length());
     }
 
      /** @test */
@@ -85,43 +88,35 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     // 	$this->assertTrue($this->router->match($this->route_2->getURL(), $this->routes[2]), "The Route does not match");
     // 	$this->assertTrue($this->router->match($this->route_3->getURL(), $this->routes[3]), "The Route does not match");
     // 	$this->assertTrue($this->router->match($this->route_4->getURL(), $this->routes[4]), "The Route does not match");
-
-
     // }
 
     /** @test */
     public function shouldReturnARouteNotFound()
     {
-
         $this->assertFalse($this->router->match($this->route_1->getURL(),$this->routes[4]),"The Route is Matched");
-    
     }
 
       /** @test */
     public function shouldReturn3AsAParameter()
-    {
-        
-        $this->assertEquals(3, $this->router->match($this->route_1->getURL(),$this->routes[1])['id']);
+    {     
+        $this->assertEquals(self::THREE_ELEMENTS, $this->router->match($this->route_1->getURL(),$this->routes[1])['id']);
     }
 
         /** @test */
     public function shouldReturn1AsAPost()
     {
-      
-        $this->assertEquals(1, $this->router->match($this->route_2->getURL(),$this->routes[2])['id']);
+        $this->assertEquals(self::ONE_ELEMENT, $this->router->match($this->route_2->getURL(),$this->routes[2])['id']);
     }
 
         /** @test */
     public function shouldReturnUnaCategoriaAndUnProductoWheURLisVariable()
     {
-      
         $this->assertEquals(["category"=>"una_categoria", "product" =>"un_producto"], $this->router->match($this->route_3->getURL(),$this->routes[3]));
     }
 
            /** @test */
     public function shouldReturnUnProductoWhenURLisAFixedTag()
     {
-      
         $this->assertEquals(["name"=>"un_producto"], $this->router->match($this->route_4->getURL(),$this->routes[4]));
     }
 
